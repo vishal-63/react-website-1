5f2ad8306ea3c2c85a29ab3244d3bbc58288c387
Vishal Shah <shahvishal662@gmail.com>
2021-01-22 19:04:45 +0530

    Added Home page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <Switch path="/" exact />
+        <Switch>
+          <Route path="/" exact component={Home} />
+        </Switch>
       </Router>
     </>
   );
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -27,7 +27,7 @@ function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <div class="navbar-container">
+        <div className="navbar-container">
           <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
             TRVL <i className="fab fa-typo3"></i>
           </Link>
