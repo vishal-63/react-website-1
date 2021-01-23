fa6e61348c6d13954a147f72154f221651265766
Vishal Shah <shahvishal662@gmail.com>
2021-01-23 15:03:27 +0530

    Other Pages added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Components/Pages/Home";
+import Services from "./Components/Pages/Services";
+import Products from "./Components/Pages/Products";
+import SignUp from "./Components/Pages/SignUp";
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/services" exact component={Services} />
+          <Route path="/products" exact component={Products} />
+          <Route path="/sign-up" exact component={SignUp} />
         </Switch>
       </Router>
     </>
--- src/Components/Pages/Home.jsx
+++ src/Components/Pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "../../App.css";
+import Cards from "../Cards";
 import HeroSection from "../HeroSection";
 
 function Home() {
-  return <HeroSection />;
+  return (
+    <>
+      <HeroSection />
+      <Cards />
+    </>
+  );
 }
 
 export default Home;
